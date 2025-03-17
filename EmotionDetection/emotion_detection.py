1fc745adfe171ad98065b99f53a78672f80ab6d3
Haroun-corp <harouncorp@gmail.com>
2025-03-17 11:45:38 -0400

    Initial commit

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -13,11 +13,36 @@ def emotion_detector(text_to_analyze):
     # Parsing the JSON response from the API
     formatted_response = json.loads(response.text)
 
-    emotion = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotion, key=emotion.get)
-    emotion['dominant_emotion'] = dominant_emotion
+    # If the response status code is 200, extract the emotions scores and dominant emotion from the response
+    if response.status_code == 200:
+        # Extracting emotions, score and dominant emotion from the response
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
 
-    
-    # Returning a dictionary containing emotion detection results
-    return emotion
-   
+        # extracting dominant emotion from response
+        emotion = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion, key=emotion.get)
+        emotion['dominant_emotion'] = dominant_emotion
+        dominant_emotion = emotion['dominant_emotion']
+    # If the response status code is 400, set emotions scores and dominant emotion to None
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
+
+    # Returning a dictionary containing emotions scores and dominant emotion results
+    return {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+        "dominant_emotion": dominant_emotion 
+        }
+        
--- server.py
+++ server.py
@@ -12,23 +12,27 @@ def render_index_page():
 
 @app.route("/emotionDetector")
 def sent_analyzer():
-    """Return the emotions score and the dominant emotion."""
+    """Return the emotions scores and the dominant emotion."""
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
 
-    # Extract the emotion and score from the response
-    anger = response['anger']
-    disgust = response['disgust']
-    fear = response['fear']
-    joy = response['joy']
-    sadness = response['sadness']
+    # Extract the emotions scores and dominant emotion from the response
+    anger_score = response['anger']
+    disgust_score = response['disgust']
+    fear_score = response['fear']
+    joy_score = response['joy']
+    sadness_score = response['sadness']
     dominant_emotion = response['dominant_emotion']
 
-    # Return a formatted string with the emotion, score and dominant_emotion 
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    # Check if the emotion scores is None and dominant emotion , indicating an error or invalid input
+    if anger_score is None and disgust_score is None and fear_score is None and joy_score is None and sadness_score is None and dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
+    # Return a formatted string with the emotions, scores and dominant_emotion 
+    return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score}, and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
